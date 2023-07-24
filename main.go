342fdc236618663df97b3a0629bcc66ac30922a9
Rahmat Wibowo <rahmat.wibowo.itb@gmail.com>
2023-07-25 06:30:26 +0700

    try to publish module

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/rweebs/cdc-bowo/src/cmd"
+
+// main is the entry point for the command line interface. It will be called from the command line with no arguments
+func main() {
+	cmd.Execute()
+}
